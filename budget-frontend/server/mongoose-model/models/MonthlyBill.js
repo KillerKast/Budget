f1a3766d36f2f37f9fcf110f57658e30587cec98
KillerKast <Jonathan.Kast312@gmail.com>
2017-06-01 08:12:39 -0400

    Adding Mongoose

---
+++ budget-frontend/server/mongoose-model/models/MonthlyBill.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jonathan on 5/31/2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema();
+
+var schema = new Schema({
+
+
+  content: {type: String, required: true},
+  user: {type: Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Message', schema);
