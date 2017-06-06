54ff0b44379234bdf3e5e0713e380e0afe6ddc79
KillerKast <Jonathan.Kast312@gmail.com>
2017-06-05 23:02:53 -0400

    Seperated Backend

---
+++ budget-backend-mongo/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var billRoutes = require('./routes/bill');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.Promise = require('bluebird');
+mongoose.connect('localhost:27017/killerkast-budget');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/bills', billRoutes);
+
+module.exports = app;
